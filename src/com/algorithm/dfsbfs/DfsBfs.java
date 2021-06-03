25d0f52c8f5b85338d88295bb8870e2f0c853d78
이준명 <mertyn@homeplus.co.kr>
2021-06-03 20:52:25 +0900

    dfs, bfs 샘플 완성

--- src/com/algorithm/dfsbfs/DfsBfs.java
+++ src/com/algorithm/dfsbfs/DfsBfs.java
@@ -1,20 +1,24 @@
 package com.algorithm.dfsbfs;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 
-class DfsBfs {
-    //공통 인접 노드 정보를 담는 맵
-    private Map<String, List<String>> adjacentMap = new LinkedHashMap<>();
-    //공통 방문 리스트
-    private List<String> visitedList = new LinkedList<>();
-    //bfs queue
-    private Queue<String> queue = new LinkedList<>();
+class DfsBfs<E> {
 
-    void addEdge(String key, String adjacent) {
+    //공통 인접 노드 정보를 담는 맵
+    private Map<E, List<E>> adjacentMap = new LinkedHashMap<>();
+    //공통 방문 리스트
+    private List<E> visitedList = new LinkedList<>();
+    //bfs queue
+    private Queue<E> queue = new LinkedList<>();
+    //result List
+    private List<E> result = new ArrayList<>();
+
+    void addEdge(E key, E adjacent) {
         //키값이 존재하면 기존의 값에 append, 존재 하지 않으면 새로 put
         if(adjacentMap.containsKey(key)){
             adjacentMap.get(key).add(adjacent);
@@ -26,14 +30,14 @@ class DfsBfs {
     }
 
     // dfs function
-    void dfs(String key) {
+    void dfs(E key) {
         // 방문 추가
         visitedList.add(key);
-        // 출력
-        System.out.print(key + " ");
+        // 결과리스트 추가
+        result.add(key);
 
         // recursion loop
-        for (String nKey : adjacentMap.get(key)) {
+        for (E nKey : adjacentMap.get(key)) {
             if (!visitedList.contains(nKey)) {
                 dfs(nKey);
             }
@@ -41,17 +45,18 @@ class DfsBfs {
     }
 
     // bfs function
-    void bfs(String key){
+    void bfs(E key){
         queue.add(key);         // 큐 추가
         visitedList.add(key);   // 방문 추가
 
         // queue loop
         while(queue.size() != 0){
-            String queueKey = queue.poll();
-            System.out.print(queueKey + " ");
+            E queueKey = queue.poll();
+            //결과 list 추가
+            result.add(queueKey);
 
             // list loop
-            for (String iterKey : adjacentMap.get(queueKey)) {
+            for (E iterKey : adjacentMap.get(queueKey)) {
                 if (!visitedList.contains(iterKey)) {
                     visitedList.add(iterKey);
                     queue.add(iterKey);
@@ -62,6 +67,11 @@ class DfsBfs {
 
     void init(){
         visitedList.clear();
+        result.clear();
+    }
+
+    List<E> getResult(){
+        return this.result;
     }
 
     public static void main(String args[]) {
@@ -80,7 +90,7 @@ class DfsBfs {
          *  E = X
          *  F = [E]
          */
-        DfsBfs dfsBfs = new DfsBfs();
+        DfsBfs<String> dfsBfs = new DfsBfs<>();
         // 노드 설정
         dfsBfs.addEdge("A", "F");
         dfsBfs.addEdge("A", "D");
@@ -91,9 +101,12 @@ class DfsBfs {
         dfsBfs.addEdge("E", "E");
         dfsBfs.addEdge("F", "E");
         dfsBfs.dfs("A");
+        // Run DFS
+        System.out.println(dfsBfs.getResult());
         dfsBfs.init();
 
-        System.out.println();
+        // Run BFS
         dfsBfs.bfs("A");
+        System.out.println(dfsBfs.getResult());
     }
 }
