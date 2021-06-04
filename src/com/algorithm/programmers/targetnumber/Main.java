a7972cd5bd26ec51d58aa5476cfce359f482f7b9
이준명 <mertyn@homeplus.co.kr>
2021-06-04 15:32:21 +0900

    https://programmers.co.kr/learn/courses/30/lessons/43165?language=java
    타겟넘버

--- src/com/algorithm/programmers/targetnumber/Main.java
+++ src/com/algorithm/programmers/targetnumber/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args){
         Solution solution = new Solution();
+        // [1, -1, 3, 1, -1, -3, 1, -1, 3, 1, 5, 3, 1, -1, 3, 1, -1, -3, 1, -1, -3, -5, -1, -3, 1, -1, 3, 1, -1, -3, 1, -1]
         solution.solution(new int[]{1,1,1,1,1}, 3);
-        // [1, 1, 1, 1, 1]	3	5
     }
 }
