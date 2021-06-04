496a99ba25ee01afeb5425d228abd7a29fee59bd
이준명 <mertyn@homeplus.co.kr>
2021-06-04 15:15:05 +0900

    https://programmers.co.kr/learn/courses/30/lessons/43165?language=java
    타겟넘버

---
+++ src/com/algorithm/programmers/targetnumber/Main.java
@@ -0,0 +1,10 @@
+package com.algorithm.programmers.targetnumber;
+
+public class Main {
+
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        solution.solution(new int[]{1,1,1,1,1}, 3);
+        // [1, 1, 1, 1, 1]	3	5
+    }
+}
---
+++ src/com/algorithm/programmers/targetnumber/Solution.java
@@ -0,0 +1,58 @@
+package com.algorithm.programmers.targetnumber;
+
+import java.util.Stack;
+
+class Solution {
+
+    /*
+        https://programmers.co.kr/learn/courses/30/lessons/43165?language=java
+        최단거리가 아니라 경우의 수를 구하는것이므로 DFS를 활용한다.
+        제공되는 배열은 정수뿐이므로 하나의 숫자를 +와 -값이 있다고 생각해야한다.
+        한 depth에 만들어진 리스트를 -를 포함한 값으로 *2 만큼의 개수를 생성한다
+        최초 실행은 0으로 시작해서 제공되는 배열의 첫글자도 +, -가 존재하게끔 한다..?
+        깊이의 값이 유니크하지 않네... 다른 처리가 필요
+
+
+
+        loop당 스택이 비워지면 하나의 뎁스처리가 종료된것으로 간주한다.
+            - 다 비우지 않고 계산할 때에 뒤죽박죽...;;
+        하나의 연산 stack의 결과마다 +와 -의 값을 연산한값을 넣는다.
+            - 최종 깊이의 연산값은 2의 n승
+        전체 연산 결과중, target 숫자와 같은 값의 개수를 stream연산으로 return 한다.
+     */
+        /*
+                                         [0](Z)                               // 1    / 2의 0승
+                  +1(A)                                     -1(H)             // 2    / 2의 1승
+            +1(B)      -1(C)                        +1(I)       -1(J)         // 4    / 2의 2승
+          +1(D)  -1(E)  +1(F)   -1(G)         +1(K)  -1(L)   +1(M)  -1(N)     // 8    / 2의 3승
+
+          제공받은 numbers의 개수 = n
+          2의 n승
+          1 1 1 1 1 = 32 개수
+
+          1 뎁스 / 0 = 1, -1
+            [0,1][0,-1]
+          2 뎁스 / 1 = 1, -1 | -1 = 1, -1
+            [1,1][1,-1][-1,1][-1,-1]
+          3 뎁스 / 1 = 1, -1 | -1
+            [1,1][1,-1][-1,+1][-1,-1][1,1][1,-1][-1,1][-1,-1]
+*/
+
+    private Stack<Integer> stack = new Stack<>();
+    public int solution(int[] numbers, int target) {
+        //스택넣고 방문 표시
+        stack.push(0);
+        for (int number : numbers) {
+            Stack<Integer> tempStack = new Stack<>();
+            //tempStack.addAll(stack);
+
+            while (!stack.empty()) {
+                int value = stack.pop();
+                tempStack.push(number + value);
+                tempStack.push((number * -1) + value);
+            }
+            stack.addAll(tempStack);
+        }
+        return (int) stack.stream().filter(num -> num == target).count();
+    }
+}
