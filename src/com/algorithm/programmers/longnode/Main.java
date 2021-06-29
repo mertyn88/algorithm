2abda00f5bb12c88b7f08b8888999c763c28bd2d
이준명 <mertyn@homeplus.co.kr>
2021-06-29 13:51:12 +0900

    프로그래머스 - 가장먼노드

--- src/com/algorithm/programmers/longnode/Main.java
+++ src/com/algorithm/programmers/longnode/Main.java
@@ -2,9 +2,9 @@ package com.algorithm.programmers.longnode;
 
 public class Main {
     public static void main(String[] args){
-        Solution2 solution = new Solution2();
+        Solution solution = new Solution();
         //6	[[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]	3
-
-        solution.solution(6, new int[][]{{3,6}, {4,3}, {3,2}, {1,3}, {1,2}, {2,4}, {5,2}});
+        System.out.println(solution.solution(6, new int[][]{{3,6}, {4,3}, {3,2}, {1,3}, {1,2}, {2,4}, {5,2}}));
+        ;
     }
 }
--- src/com/algorithm/programmers/longnode/Solution.java
+++ src/com/algorithm/programmers/longnode/Solution.java
@@ -1,101 +1,94 @@
 package com.algorithm.programmers.longnode;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Stack;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Queue;
+import java.util.Set;
 
 public class Solution {
 
-    //6	[[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]	3
-    // 36 43 32 13 12 24 52
 
-    private Stack<Integer> stack = new Stack<>();
-    List<String> edgeList = new ArrayList<>(){{
-        add("36");
-        add("43");
-        add("32");
-        add("13");
-        add("12");
-        add("24");
-        add("52");
-    }};
-    Map<String, int[]> edgeMap = new HashMap<>(){{
-        put("36", new int[]{3,6});
-        put("43", new int[]{4,3});
-        put("32", new int[]{3,2});
-        put("13", new int[]{1,3}); // 1
-        put("12", new int[]{1,2});
-        put("24", new int[]{2,4});
-        put("52", new int[]{5,2});
-    }};
-    Map<Integer, Integer> result = new HashMap<>();
+    //공통 인접 노드 정보를 담는 맵
+    private Map<Integer, Set<Integer>> adjacentMap = new HashMap<>();
+    //공통 방문 리스트
+    private List<Integer> visitedList = new LinkedList<>();
+    //bfs queue
+    private Queue<Integer> queue = new LinkedList<>();
 
-    Map<Integer, List<Integer>> resultMap = new HashMap<>();
-
-
-
-    void dfsStack(String key, int node, List<String> visitList, int count){
-
-        Map<String, int[]> tempMap = edgeMap.entrySet().stream().filter( entry -> !visitList.contains(entry.getKey()) )  // 방문 리스트에 없고
-            .filter( entry -> Arrays.stream(entry.getValue()).anyMatch(a -> a == node))  // value 중 node값이 있는거..
-            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-
-        System.out.println(key + " " + node + " " + tempMap);
-        System.out.println(visitList);
-        System.out.println("-----------");
-
-        if(tempMap.isEmpty()){
-            // 제일 멀리 있는거..
-            System.out.println("result Node > " + node + " " + count);
-            result.merge(node, count, Math::min);
-
-            resultMap.merge(node, new ArrayList<>(){{add(count);}}, (i, h) -> new ArrayList<>(){{addAll(i);addAll(h);}});
-        }else{
-            for(String k : tempMap.keySet()){
-
-                // TODO 결국 여기서 서로 엮여있으면 제외를 체크해야한다.
-                // j의 인접노드가 1이 없어야한다. 1이 있으면 서로 엮인
-                if(tempMap.get(k)[0] != 1){
-                    System.out.println(k);
-                    resultMap.merge(tempMap.get(k)[0], new ArrayList<>(){{add(count);}}, (i, h) -> new ArrayList<>(){{addAll(i);addAll(h);}});
-                }
-
-                for(int j : tempMap.get(k)){
-                    if(j != node){
-
-
-                        System.out.println(j + " 최단거리구하기 " + count );
-                        System.out.println(visitList);
-                        dfsStack(k, j, new ArrayList<>(){{addAll(visitList); add(k);}}, count + 1);
-                    }
+    // 시간초과 이슈 있었던 코드
+    @Deprecated
+    public void tempSetData(int n, int[][] edge){
+        for(int idx = 1; idx <= n; idx++){
+            for (int[] arr : edge) {
+                if (idx == arr[0]) {
+                    addEdge(idx, arr[1]);
+                } else if (idx == arr[1]) {
+                    addEdge(idx, arr[0]);
                 }
             }
         }
     }
+    // 시간초과 해결한 코드
+    public void setData(int[][] edge){
+        for (int[] arr : edge) {
+            addEdge(arr[0], arr[1]);
+            addEdge(arr[1], arr[0]);
+        }
+    }
 
+    void addEdge(int key, int adjacent) {
+        //키값이 존재하면 기존의 값에 append, 존재 하지 않으면 새로 put
+        if(adjacentMap.containsKey(key)){
+            adjacentMap.get(key).add(adjacent);
+        }else{
+            adjacentMap.put(key, new HashSet<>(){{
+                add(adjacent);
+            }});
+        }
+    }
+
+    public int bfs(){
+        int answer = 0;
+
+        // Switching Queue variable
+        Queue<Integer> tempQueue = new LinkedList<>();
+        // queue loop
+        while(queue.size() != 0){
+            int queueKey = queue.poll();
+
+            // adjacent list loop
+            for (int loopKey : adjacentMap.get(queueKey)) {
+                if (!visitedList.contains(loopKey)) {
+                    visitedList.add(loopKey);
+                    tempQueue.add(loopKey);
+                }
+            }
+
+            if(queue.size() == 0){
+                //뎁스별 노드 삽입
+                if(tempQueue.size() != 0){
+                    answer = tempQueue.size();  // 마지막 tempQueue 사이즈가 마지막 뎁스의 노드 개수
+
+                    queue.addAll(tempQueue);
+                    tempQueue.clear();
+                }
+            }
+        }
+
+        return answer;
+    }
 
     public int solution(int n, int[][] edge) {
-        List<String> visitList = new ArrayList<>();
-        //visitList.add("13");
-        int count = 1;
-        // 1번으로 시작해서 이걸 찾은거
-        dfsStack("", 1, visitList, count);
-        //dfsStack("12");
+        ////6   [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
+        // 시간초과 이슈
+        //tempSetData(n, edge);
+        setData(edge);
 
-        // {1=3, 5=3, 6=2}
-        // {1=3, 5=2, 6=2}
-
-        System.out.println(result);
-        System.out.println(resultMap);
-
-
-        int answer = 0;
-        return answer;
+        visitedList.add(1);
+        queue.add(1);         // 큐 추가
+        return bfs();
     }
 }
