e6af202a8faad70c25193b0124ce872c1b48049d
이준명 <mertyn@homeplus.co.kr>
2021-07-16 18:14:49 +0900

    프로그래머스 - 카카오 - 자물쇠와 열쇠

---
+++ src/com/algorithm/kakao/lock/Main.java
@@ -0,0 +1,15 @@
+package com.algorithm.kakao.lock;
+
+
+public class Main {
+    public static void main(String[] args) {
+       Solution solution = new Solution();
+       // [[0, 0, 0], [1, 0, 0], [0, 1, 1]]	[[1, 1, 1], [1, 1, 0], [1, 0, 1]]	true
+       System.out.println(
+           solution.solution(
+               new int[][]{{0,0,0}, {1,0,0}, {0,1,1}},
+               new int[][]{{1,1,1}, {1,1,0}, {1,0,1}}
+           )
+       );
+    }
+}
