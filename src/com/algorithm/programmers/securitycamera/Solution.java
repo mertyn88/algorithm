0cc92ac3509b5c2ea59c10f6af6b437622784be5
이준명 <mertyn@homeplus.co.kr>
2021-07-28 09:38:10 +0900

    프로그머스 - 단속카메라

--- src/com/algorithm/programmers/securitycamera/Solution.java
+++ src/com/algorithm/programmers/securitycamera/Solution.java
@@ -7,15 +7,12 @@ class Solution {
         int answer = 0;
         // 정렬
         //int[][] sortArr = Arrays.stream(routes).sorted(Comparator.comparing(route -> route[1])).toArray(int[][]::new);
-
         Arrays.sort(routes, (o1, o2) -> {
             if(o1[1] == o2[1])
                 return Integer.compare(o1[0], o2[0]);
             else
                 return Integer.compare(o1[1], o2[1]);
         });
-
-
 
         // 방문 리스트
         boolean[] isVisit = new boolean[routes.length];
