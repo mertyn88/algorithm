c8ee87631868b9983dba56f3b8a81cbd78f6a868
이준명 <mertyn@homeplus.co.kr>
2021-07-30 08:46:13 +0900

    프로그머스 - 섬연결

--- src/com/algorithm/programmers/connectisland/Solution.java
+++ src/com/algorithm/programmers/connectisland/Solution.java
@@ -13,46 +13,50 @@ public class Solution {
             else
                 return Integer.compare(o1[2], o2[2]);
         });
-        for (int[] temp : costs) {
-            for (int b : temp) {
-                System.out.print(b + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-
         // 방문 벡터 배열
         int[] visitArr = new int[n];
         Arrays.fill(visitArr, -1);
         // 자식노드 부모로 변경?
-
-
 
         int answer = 0;
         for (int[] cost : costs) {
             // 최상위 부모를 찾는다.
             // cost의 0번째 값이 visitArr에서 -1값이 아니면 그값을 탐색한다.
             // -1 값이발견되면 그값은 아직 할당되지 않았으므로 작성가
-            int root = findParent(cost[0], visitArr);
-            visitArr[cost[1]] = root;
-            //root값이 기존값과 동일한데 이미 자식노드에 값이 있는경우?
-            if(cost[0] == root && visitArr[cost[1]] != -1){
+
+            /*
+                ** 각 시작, 끝 별로 찾아간 root의 값이 같은지 틀린지를 비교하고 서로 값이 틀리면
+                * 두 값을 비교 하여 큰값일때는 큰값의 위치에 작은값을 설정하고, 작은값일때는 작은값의 위치에
+                *
+                * 크리스컬 알고리즘
+             */
+
+
+            int startRoot = findRoot(cost[0], visitArr);
+            int endRoot = findRoot(cost[1], visitArr);
+
+            if(startRoot == endRoot){
+                continue;
+            }else{
+                // 큰값을 작은값으로 치환
+                if(startRoot > endRoot){
+                    visitArr[startRoot] = endRoot;
+                }else{
+                    visitArr[endRoot] = startRoot;
+                }
                 answer += cost[2];
             }
         }
-
-        System.out.println(">> " + answer);
-
-
-        //System.out.println("------");
         return answer;
     }
 
-   private int findParent(int child, int[] visitArr) {
-       if (visitArr[child] == -1) {
+   private int findRoot(int child, int[] visitArr) {
+       if(child == visitArr[child]){
+           return child;
+       }else if (visitArr[child] == -1) {
            return child; // 기존 부모값 사용
        } else {
-           return findParent(visitArr[child], visitArr);
+           return findRoot(visitArr[child], visitArr);
        }
    }
 }
