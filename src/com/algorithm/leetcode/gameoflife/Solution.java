32e9295221fd30203ada98fd52e02577334278b2
이준명 <mertyn@homeplus.co.kr>
2021-09-03 17:24:12 +0900

    리트코드 - 게임오브라이프

--- src/com/algorithm/leetcode/gameoflife/Solution.java
+++ src/com/algorithm/leetcode/gameoflife/Solution.java
@@ -19,31 +19,27 @@ public class Solution {
 
         for(int x = 0; x < sizeX; x++){
             for(int y = 0; y < sizeY; y++){
-                if(isRangeCheck(x, y)){
-
-                }
+                setCell(board, result, x, y);
             }
         }
 
+        //print(result);
 
-
-        for(int[] a : result){
-            for(int b : a){
-                System.out.print(b + " ");
-            }
-            System.out.println();
+        for(int i = 0; i < sizeX; i++){
+            board[i] = result[i].clone();
         }
+        print(board);
     }
 
     private boolean isRangeCheck(int x, int y){
         // x,y 값이 최대길이를 벗어날경우
-        if(x > sizeX || y > sizeY) {
+        if(x > sizeX - 1 || y > sizeY - 1) {
             return false;
         }
         return x >= 0 && y >= 0;
     }
 
-    private void setCell(int[][] result, int x, int y){
+    private void setCell(int[][] board, int[][] result, int x, int y){
         // 8방향 정의
         int[][] direction = new int[][]{
             {-1,-1},
@@ -56,10 +52,30 @@ public class Solution {
             {1,  1}
         };
 
+        int count = 0;
         for(int[] dir : direction) {
-            //TODO 주의를 순회하며 1의 카운팅을 찾고 규칙을 적용
             if(isRangeCheck(x + dir[0], y + dir[1])){
+               if(board[x + dir[0]][y + dir[1]] == 1){
+                   count++;
+               }
             }
+        }
+
+        // 살아있는 세포라면 이웃카운트가 2 ~ 3개만 허용
+        if(board[x][y] == 1 && (count == 2 || count == 3)){
+            result[x][y] = 1;
+        }else if(board[x][y] == 0 && count == 3){
+            // 죽어 있는 세포라면 이웃카운트가 3개일 경우에만 살아남
+            result[x][y] = 1;
+        }
+    }
+
+    private void print(int[][] result) {
+        for(int[] a : result){
+            for(int b : a){
+                System.out.print(b + " ");
+            }
+            System.out.println();
         }
     }
 }
